3a020d2c4c5d56aca706119e52833a599c5d328a
Kacper Kiełbasa <kacper@MacBook-Air-Kacper.local>
2025-05-14 22:49:19 +0200

    dodanie klasy bazowej figur

--- pieces/piece.py
+++ pieces/piece.py
@@ -1,1 +1,13 @@
 # klasa bazowa dla wszytkich figur
+
+class Figura:
+    def __init__(self, kolor, pozycja):
+        self.kolor = kolor  # Atrybut przechowujący kolor figury.
+        self.pozycja = pozycja  # Atrybut przechowujący pozycję figury.
+
+    def mozliwe_ruchy(self, plansza):
+        return []  # Metoda zwracająca listę możliwych ruchów (domyślnie pusta - do nadpisania).
+
+    def __str__(self):
+        return f"{self.kolor[0].upper()}{self.__class__.__name__[0]}"  
+	# Metoda zwracająca tekstową reprezentację figury (pierwsza litera koloru i nazwy klasy).
