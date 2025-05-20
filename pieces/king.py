e3bcbf545434cede56189a9c2a44ff72ade329fb
IamSzymeQQQ <142638357+IamSzymeQQQ@users.noreply.github.com>
2025-05-20 11:54:14 +0200

    Dodano figurę król (na razie bez funkcji loop).py

---
+++ pieces/king.py
@@ -0,0 +1,8 @@
+class Krol(Figura):
+    def mozliwe_ruchy(self, plansza):
+        ruchy = []
+        rzad, kolumna = self.pozycja
+      
+        #tutaj trzeba jeszcze for loopa dodac
+      
+        return ruchy
