1cdddcf5ee7d3c457a094ff64dc554b7ae1d2b4d
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-05-20 12:31:27 +0200

    Rozpoczecie pracy nad rook.py

---
+++ pieces/rook.py
@@ -0,0 +1,16 @@
+class Wieza(Figura):
+    # Metoda zwracająca listę możliwych ruchów dla wieży
+    def mozliwe_ruchy(self, plansza):
+        ruchy = []  # Inicjalizacja pustej listy na możliwe ruchy
+        rzad, kolumna = self.pozycja  # Pobranie aktualnej pozycji wieży (rząd i kolumna)
+
+
+	# TO DO:
+
+        # Definicja kierunków ruchu dla wieży: (0, 1) - prawo, (0, -1) - lewo, (1, 0) - dół, (-1, 0) - góra
+        # Pętla iterująca przez pola w danym kierunku (od 1 do 7 pól od wieży)
+        # Obliczenie współrzędnych kolejnego pola
+        # Sprawdzenie, czy obliczone współrzędne mieszczą się w granicach planszy (0-7 dla rzędów i kolumn)
+
+
+        return ruchy  # Zwróć listę wszystkich możliwych ruchów
