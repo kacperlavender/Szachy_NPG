6f36cc8011b9879b99b3af97d830eb411908e6ff
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-05-23 00:02:14 +0200

    dodanie kierunkow ruchu wierzy

--- pieces/rook.py
+++ pieces/rook.py
@@ -3,11 +3,12 @@ class Wieza(Figura):
     def mozliwe_ruchy(self, plansza):
         ruchy = []  # Inicjalizacja pustej listy na możliwe ruchy
         rzad, kolumna = self.pozycja  # Pobranie aktualnej pozycji wieży (rząd i kolumna)
-
+        # Definicja kierunków ruchu dla wieży: (0, 1) - prawo, (0, -1) - lewo, (1, 0) - dół, (-1, 0) - góra
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            for i in range(1, 8):
 
 	# TO DO:
 
-        # Definicja kierunków ruchu dla wieży: (0, 1) - prawo, (0, -1) - lewo, (1, 0) - dół, (-1, 0) - góra
         # Pętla iterująca przez pola w danym kierunku (od 1 do 7 pól od wieży)
         # Obliczenie współrzędnych kolejnego pola
         # Sprawdzenie, czy obliczone współrzędne mieszczą się w granicach planszy (0-7 dla rzędów i kolumn)
