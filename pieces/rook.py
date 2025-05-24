4c3e199831db3d13e4c1c469d6e53b164e857c19
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-05-24 11:01:57 +0200

    Dodanie logiki zbicia dla wiezy

--- pieces/rook.py
+++ pieces/rook.py
@@ -1,17 +1,28 @@
 class Wieza(Figura):
-    # Metoda zwracająca listę możliwych ruchów dla wieży
+    # Metoda do wyliczania możliwych ruchów wieży
     def mozliwe_ruchy(self, plansza):
-        ruchy = []  # Inicjalizacja pustej listy na możliwe ruchy
-        rzad, kolumna = self.pozycja  # Pobranie aktualnej pozycji wieży (rząd i kolumna)
-        # Definicja kierunków ruchu dla wieży: (0, 1) - prawo, (0, -1) - lewo, (1, 0) - dół, (-1, 0) - góra
+        ruchy = []  # Lista na legalne ruchy
+        rzad, kolumna = self.pozycja  # Aktualna pozycja wieży
+
+        # Iteruj przez cztery kierunki: prawo, lewo, dół, góra
         for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            # Sprawdź pola w danym kierunku, od 1 do 7 pól od wieży
             for i in range(1, 8):
+                nr, nc = rzad + i * dr, kolumna + i * dc  # Oblicz nową pozycję
 
-	# TO DO:
-
-        # Pętla iterująca przez pola w danym kierunku (od 1 do 7 pól od wieży)
-        # Obliczenie współrzędnych kolejnego pola
-        # Sprawdzenie, czy obliczone współrzędne mieszczą się w granicach planszy (0-7 dla rzędów i kolumn)
-
-
+                # Sprawdź, czy nowa pozycja jest na szachownicy
+                if 0 <= nr < 8 and 0 <= nc < 8:
+                    # Jeśli pole jest puste, dodaj je do możliwych ruchów
+                    if plansza.pola[nr][nc] is None:
+                        ruchy.append((nr, nc))
+                    # Jeśli na polu jest figura przeciwnika, dodaj ruch zbicia i zatrzymaj się w tym kierunku
+                    elif plansza.pola[nr][nc].kolor != self.kolor:
+                        ruchy.append((nr, nc))
+                        break
+                    # Jeśli na polu jest własna figura, wieża jest zablokowana w tym kierunku
+                    else:
+                        break
+                # Jeśli nowa pozycja wykracza poza szachownicę, zakończ sprawdzanie w tym kierunku
+                else:
+                    break
         return ruchy  # Zwróć listę wszystkich możliwych ruchów
