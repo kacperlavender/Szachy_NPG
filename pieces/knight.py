b77d61e89427c61483df4df21b15ac6826271b4c
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-05-25 11:32:45 +0200

    rozpoczecie pracy nad skoczkiem

---
+++ pieces/knight.py
@@ -0,0 +1,17 @@
+class Skoczek(Figura):
+    def mozliwe_ruchy(self, plansza):
+        # Inicjalizacja listy przechowującej możliwe ruchy skoczka
+        ruchy = []
+        # Pobranie aktualnej pozycji skoczka (wiersz, kolumna)
+        rzad, kolumna = self.pozycja
+ 
+	# todo
+       	    # Wszystkie możliwe kierunki ruchu skoczka (kształt litery 'L')
+            # Nowa pozycja po ruchu
+            # Sprawdzenie, czy nowa pozycja jest na planszy (8x8)
+            # oraz czy pole jest puste lub przeciwnik (można bić)
+            # Jeśli warunek spełniony, dodaj ruch do listy
+        
+        # Zwróć listę możliwych ruchów
+        return ruchy
+
