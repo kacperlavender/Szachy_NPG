3cf5e8950ec28865841cdcb8174aa1bfbee3094a
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-05-26 18:44:40 +0200

    Dalszy rozwoj skoczka

--- pieces/knight.py
+++ pieces/knight.py
@@ -4,14 +4,21 @@ class Skoczek(Figura):
         ruchy = []
         # Pobranie aktualnej pozycji skoczka (wiersz, kolumna)
         rzad, kolumna = self.pozycja
- 
-	# todo
-       	    # Wszystkie możliwe kierunki ruchu skoczka (kształt litery 'L')
+        
+        # Wszystkie możliwe kierunki ruchu skoczka (kształt litery 'L')
+        for dr, dc in [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]:
             # Nowa pozycja po ruchu
+            nr, nc = rzad + dr, kolumna + dc
+            
             # Sprawdzenie, czy nowa pozycja jest na planszy (8x8)
             # oraz czy pole jest puste lub przeciwnik (można bić)
-            # Jeśli warunek spełniony, dodaj ruch do listy
+            if 0 <= nr < 8 and 0 <= nc < 8 and (
+                plansza.pola[nr][nc] is None or plansza.pola[nr][nc].kolor != self.kolor
+            ):
+                # Jeśli warunek spełniony, dodaj ruch do listy
+                ruchy.append((nr, nc))
         
         # Zwróć listę możliwych ruchów
         return ruchy
+
 
